721ccd725073e2fcb1c8b7abc2bbfe9373cbb14c
whiterhc <whiterhc729@gmail.com>
2022-12-13 10:53:29 +0900

    - mongoDB 연동한 REST API (GET, DELETE, PATCH) 개발

--- src/backend/api/polls/index.js
+++ src/backend/api/polls/index.js
@@ -3,6 +3,10 @@ import * as pollsCtrl from './polls.ctrl.js';
 
 const polls = new Router();
 
+polls.get('/', pollsCtrl.list);
 polls.post('/', pollsCtrl.write);
+polls.get('/:id', pollsCtrl.read);
+polls.delete('/:id', pollsCtrl.remove);
+polls.patch('/:id', pollsCtrl.update);
 
 export default polls;
--- src/backend/api/polls/polls.ctrl.js
+++ src/backend/api/polls/polls.ctrl.js
@@ -1,6 +1,19 @@
 import Poll from '../../models/poll.js';
 
 /**
+ *  GET /api/polls
+ *
+ * */
+export const list = async ctx => {
+	try {
+		const polls = await Poll.find().exec();
+		ctx.body = polls;
+	} catch (e) {
+		ctx.throw(500, e);
+	}
+}
+
+/**
  *  POST /api/polls
  *  {
  *      title,
@@ -21,3 +34,48 @@ export const write = async ctx => {
 		ctx.throw(500, e);
 	}
 };
+
+/**
+ *  GET /api/polls/:id
+ *
+ * */
+export const read = async ctx => {
+	const {id} = ctx.params;
+	try {
+		const poll = await Poll.findById(id).exec();
+		if (!poll) {
+			ctx.status = 404;
+			return;
+		}
+		ctx.body = poll;
+	} catch(e) {
+		ctx.throw(500, e);
+	}
+};
+
+export const remove = async ctx => {
+	const {id} = ctx.params;
+	try {
+		await Poll.findByIdAndRemove(id).exec();
+		ctx.status = 204;
+	} catch (e) {
+		ctx.throw(500, e);
+	}
+};
+
+export const update = async ctx => {
+	const {id} = ctx.params;
+	try {
+		const poll = await Poll.findByIdAndUpdate(id, ctx.request.body, {
+			new: true
+		}).exec();
+		if (!poll) {
+			ctx.status = 404;
+			return;
+		}
+		ctx.body = poll;
+	} catch (e) {
+		ctx.throw(500, e);
+	}
+};
+
